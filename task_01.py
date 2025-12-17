e09763c332b52de3f24ad4b775de3849a41c730a
Andy-Parshyn <andriy.parshyn@gmail.com>
2025-12-17 12:31:59 +0100

    task_1

---
+++ task_01.py
@@ -0,0 +1,199 @@
+'''
+Для реалізації однозв'язного списку (приклад реалізації можна взяти з конспекту) необхідно:
+
+написати функцію, яка реалізує реверсування однозв'язного списку, змінюючи посилання між вузлами;
+розробити алгоритм сортування для однозв'язного списку, наприклад, сортування вставками або злиттям;
+написати функцію, що об'єднує два відсортовані однозв'язні списки в один відсортований список.
+'''
+
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next : Node | None = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head : Node | None = None
+
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            cur = self.head
+            while cur.next:
+                cur = cur.next
+            cur.next = new_node
+
+    def insert_after(self, prev_node: Node, data):
+        if prev_node is None:
+            print("Попереднього вузла не існує.")
+            return
+        new_node = Node(data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+    def delete_node(self, key: int):
+        cur = self.head
+
+        if cur and cur.data == key:
+            self.head = cur.next
+            cur = None
+            return
+        
+        prev = None
+
+        while cur and cur.data != key:
+            prev = cur
+            cur = cur.next
+        if cur is None:
+            return
+        if prev is not None:
+            prev.next = cur.next
+        cur = None
+
+    def search_element(self, data: int) -> Node | None:
+        cur = self.head
+        while cur:
+            if cur.data == data:
+                return cur
+            cur = cur.next
+        return None
+
+    def reverse(self):
+        prev = None
+        current = self.head
+        while current:
+            next_node = current.next  
+            current.next = prev       
+            prev = current            
+            current = next_node       
+        self.head = prev              
+    
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data)
+            current = current.next
+
+    def sort(self):
+        self.head = self._merge_sort(self.head)
+
+    @staticmethod
+    def merge_lists(llist1,llist2):
+        merged_list = LinkedList()
+        dummy = Node(0)
+        tail: Node = dummy
+        p1 = llist1.head
+        p2 = llist2.head
+
+        while p1 and p2:
+            if p1.data <= p2.data:
+                tail.next = p1
+                p1 = p1.next
+            else:
+                tail.next = p2
+                p2 = p2.next
+
+            if tail.next:
+                tail = tail.next
+
+        if p1:
+            tail.next = p1
+        elif p2:
+            tail.next = p2
+        
+        merged_list.head = dummy.next
+
+        return merged_list
+            
+
+    def _merge_sort(self, head:Node|None) -> Node | None:
+        if head is None or head.next is None:
+            return head
+        
+        middle = self._get_middle(head)
+
+        if middle is None:
+            return head
+        next_to_middle = middle.next
+
+        middle.next = None
+
+        left = self._merge_sort(head)
+        right = self._merge_sort(next_to_middle)
+        sorted_list = self._sorted_merge(left,right)
+        return sorted_list
+    
+    def _get_middle(self, head: Node | None) -> Node | None:
+        if head is None:
+            return head
+        
+        slow = head
+        fast = head
+
+        while fast.next and fast.next.next:
+            if slow.next:
+                slow = slow.next
+            fast = fast.next.next
+        return slow
+        
+    def _sorted_merge(self, a: Node|None, b: Node| None) -> Node| None:
+        if a is None:
+            return b
+        if b is None:
+            return a
+        
+        result = None
+        if a.data is not None and b.data is not None and a.data <= b.data:
+            result = a
+            result .next = self._sorted_merge(a.next,b)
+        else:
+            result = b
+            result.next = self._sorted_merge(a,b.next)
+        return result
+
+
+if __name__ == '__main__':
+
+    # Перший список
+    list1 = LinkedList()
+    list1.insert_at_end(7)
+    list1.insert_at_end(60)
+    list1.insert_at_end(10)
+
+    # Другий список
+    list2 = LinkedList()
+    list2.insert_at_end(34)
+    list2.insert_at_end(3)
+    list2.insert_at_end(20)
+
+    print('\nСписок 1:')
+    list1.print_list()  
+
+    print('\nСписок 2:')
+    list2.print_list()  
+
+    # Сортуєм списки
+    list1.sort()
+    list2.sort()
+    print('\nСортований список 1: ')
+    list1.print_list()
+    print('\nСортований список 2: ')
+    list2.print_list()
+
+    # Об'єднуємо
+    merged = LinkedList.merge_lists(list1, list2)
+
+    print('\nОб\'єднаний список:')
+    merged.print_list()
+
+    merged.reverse()
+    print('\nЗв\'язний список after reverse:')
+    merged.print_list()
